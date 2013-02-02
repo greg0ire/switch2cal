b45dd0f5519a180b7865ce4d60d960c2f0ec0b81
Grégoire Paris <postmaster@greg0ire.fr>
2013-02-02 13:27:52 +0100

    test late end date is rounded

--- tests/testSwitch2Cal.py
+++ tests/testSwitch2Cal.py
@@ -69,6 +69,42 @@ class TestSwitch2CalFunctions(unittest.TestCase):
             ))
         self.assertEquals(cleanedPeriod['end_date'], period['end_date'])
 
+    def testRoundLateEndDate(self):
+        period = {
+            'name': 'testProject',
+            'start_date': datetime(
+                year   = 2000,
+                month  = 1,
+                day    = 1,
+                hour   = 10,
+                minute = 00,
+                second = 00
+            ),
+            'end_date': datetime(
+                year   = 2000,
+                month  = 1,
+                day    = 1,
+                hour   = 20,
+                minute = 10,
+                second = 30
+            )
+        }
+        cleanedEvents = switch2Cal.cleanPeriod(period)
+        # assert there was no splitting
+        self.assertEquals(len(cleanedEvents), 1)
+
+        cleanedPeriod = cleanedEvents.pop()
+        self.assertEquals(cleanedPeriod['start_date'], period['start_date'])
+        self.assertEquals(
+            cleanedPeriod['end_date'],
+            datetime(
+                year   = 2000,
+                month  = 1,
+                day    = 1,
+                hour   = 19,
+                minute = 00,
+                second = 00
+            ))
 
 if __name__ == '__main__':
     unittest.main()
